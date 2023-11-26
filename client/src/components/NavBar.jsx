668ff145b86ab92dc0786f55dae73cc371067ef4
roskzhu <roskenzhu@gmail.com>
2023-11-26 14:59:17 -0500

    main svg -> png

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import '../styles/NavBar.css'
+import { Link, useLocation } from 'react-router-dom';
+
 
 const NavBar = () => {
+  const location = useLocation();
+
   return (
     <div className="nav-container">
       <div className="left-container">
@@ -14,16 +18,16 @@ const NavBar = () => {
       </div>
       <div className="right-container">
         <div className="right-bar">
-          <a href="/">
+        <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
             <p>
               Home
             </p>
-          </a>
-          <a href="/chat">
+          </Link>
+          <Link to="/chat" className={location.pathname === '/chat' ? 'active' : ''}>
             <p>
               Try Now
             </p>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
